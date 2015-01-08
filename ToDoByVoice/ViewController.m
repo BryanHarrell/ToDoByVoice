527c0c8a6ca86a2ededec306f41a4e58d68e1e2a
lifeofbrybry <bryanharrell@Bryan-Harrells-MacBook.local>
2015-01-08 07:36:49 +0100

    Initial

---
+++ ToDoByVoice/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ToDoByVoice
+//
+//  Created by Bryan Harrell on 16/12/2014.
+//  Copyright (c) 2014 Bryan Harrell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ToDoByVoice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoByVoice
+//
+//  Created by Bryan Harrell on 16/12/2014.
+//  Copyright (c) 2014 Bryan Harrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
